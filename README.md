b3d615053d5445fa3d519827213cc4070bb8de48
souporserious <ftntravis@gmail.com>
2021-06-06 02:49:32 -0700

    publish

---
+++ README.md
@@ -0,0 +1,10 @@
+# Bundling TypeScript With Esbuild for NPM
+
+This is an example of using Esbuild to bundle TypeScript. See [this article](https://souporserious.com/bundling-typescript-with-esbuild-for-npm/) for a walkthrough.
+
+## Development
+
+```bash
+npm install
+npm run build
+```
---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+export function Counter() {
+  const [count, setCount] = useState(0)
+  return (
+    <div>
+      <button onClick={() => setCount(count - 1)}>-</button>
+      <span>{count}</span>
+      <button onClick={() => setCount(count + 1)}>+</button>
+    </div>
+  )
+}
